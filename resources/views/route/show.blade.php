8ee1cb8e21225f81303b759e15d38eaa3f62ae29
Bernard Nandwa <nandwabee@gmail.com>
2018-07-18 17:15:04 +0300

    Update the routes display

--- resources/views/route/show.blade.php
+++ resources/views/route/show.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
+@section('body_classes')route @endsection
+
+@section('page_title')
+{{$route->route_number}}
+@endsection
+
 @section('content')
-    {{$route->route_number}}
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                {{$route->route_number}}
+            </div>
+            <div class="col-8">
+                This is the right part of the route page
+            </div>
+        </div>
+    </div>
 @endsection
