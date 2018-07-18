1629b47fa98c9d982f78442368144ca87acd6ddb
Bernard Nandwa <nandwabee@gmail.com>
2018-07-18 14:31:24 +0300

    Update homeage text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,20 @@
 @extends('layouts.app')
 
-@section('body_classes')
-dark
-@endsection
+@section('body_classes')dark @endsection
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="">
+                New to Nairobi? Looking to get around?
+            </div>
+            <div class="">
+                A matatu provides the most affordable and readily available means to move around within Nairobi.
+            </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+            <div class="">
+                We make your matatu travels even better.
             </div>
         </div>
     </div>
