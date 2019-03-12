b1012093b529f420fb439eede5ea689ec52f45e0
Bernard Nandwa <nandwabee@gmail.com>
2019-03-12 13:42:21 +0300

    Add model for tracking points of interest

---
+++ Matatu/Route/Data/Models/PointOfInterest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Used to store the data for locations across a route.
+ *
+ * Users will typically search for these kind of place when looking to get a route.
+ *
+ * @author Bernard Nandwa <bernard@nandwa.com>
+ */
+
+namespace RouteNbi\Matatu\Route\Data\Models;
+
+class PointOfInterest{
+    protected $table = 'points_of_interest';
+}
